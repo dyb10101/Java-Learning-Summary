8d4a926305c3e1e725144f3d4bf9460ab6d47856
jxnu-liguobin <568845948@qq.com>
2018-05-08 11:32:01 +0800

    LinkedList源码

--- Java-Learning-Summary/src/cn/edu/jxnu/sourcecode/package-info.java
+++ Java-Learning-Summary/src/cn/edu/jxnu/sourcecode/package-info.java
@@ -4,6 +4,7 @@ package cn.edu.jxnu.sourcecode;
  * JDK1.8
  * 
  * 参考Github的大神:wupeixuan
+ * 修改部分手误或增加补充
  * 
  * https://github.com/wupeixuan/JDKSourceCode1.8/blob/master/src/java/lang/Object.java
  * 

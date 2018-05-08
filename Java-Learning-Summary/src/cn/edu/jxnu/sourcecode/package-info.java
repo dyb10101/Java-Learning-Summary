ea5bac2114e7701d76ff66e56472964567dbaab0
jxnu-liguobin <568845948@qq.com>
2018-05-08 11:11:28 +0800

    ArrayList源码

--- Java-Learning-Summary/src/cn/edu/jxnu/sourcecode/package-info.java
+++ Java-Learning-Summary/src/cn/edu/jxnu/sourcecode/package-info.java
@@ -1,9 +1,12 @@
 package cn.edu.jxnu.sourcecode;
 
 /**
+ * JDK1.8
  * 
  * 参考Github的大神:wupeixuan
  * 
  * https://github.com/wupeixuan/JDKSourceCode1.8/blob/master/src/java/lang/Object.java
+ * 
+ * 为了不与系统类冲突，直接改了js后缀
  * 
  */

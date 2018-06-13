4913c4601f7c73a999793a0584fa0eee9257681e
jxnu-liguobin <568845948@qq.com>
2018-06-13 09:30:57 +0800

    增加访问者设计模式注释，增加设计模式定义集

--- Java-Learning-Summary/src/cn/edu/jxnu/designpattern/visitor/ConcreteElement.java
+++ Java-Learning-Summary/src/cn/edu/jxnu/designpattern/visitor/ConcreteElement.java
@@ -1,5 +1,8 @@
 package cn.edu.jxnu.designpattern.visitor;
 
+/**
+ * 具体的元素类，它提供接受访问方法的具体实现，而这个具体的实现，通常情况下是使用访问者提供的访问该元素类的方法
+ */
 public class ConcreteElement implements ElementNode {
 
 	@Override

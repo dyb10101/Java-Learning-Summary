c7a708f6b20c9bea253e184c0b10ebd527249d76
jxnu-liguobin <568845948@qq.com>
2018-06-24 20:28:13 +0800

    链表反转增加注释

--- Java-Learning-Summary/src/cn/edu/jxnu/practice/ReverseLinkedList.java
+++ Java-Learning-Summary/src/cn/edu/jxnu/practice/ReverseLinkedList.java
@@ -72,7 +72,7 @@ public class ReverseLinkedList {
 	}
 
 	/**
-	 * 第三种方法 每次都将原第一个结点之后的那个结点放在新的表头后面。
+	 * 第三种方法 每次都将原第一个结点之后的那个结点放在新的表头后面。 对12345，先保存1的下一个节点，新链表变为1345，然后把2链接在1前面即可
 	 */
 	public static ListNode reverseList3(ListNode pHead) {
 		if (pHead == null)
@@ -80,16 +80,16 @@ public class ReverseLinkedList {
 		ListNode res, first, temp;
 		res = new ListNode(-1);
 		res.next = pHead;// 无数据的头结点
-		first = res.next; // first 始终为第一个结点，不断后移
-		while (first.next != null) // temp为待前差的
+		first = res.next; // first 为第一个结点=1 带头结点的新链表：[-1,1,2,3,4,5]
+		while (first.next != null) // temp为当前等待插入的
 		{
 			// 起始，1，2，3，4，5
 			// 第一次，2，1，3，4，5
 			// 第二次，3，2，1，4，5
-			temp = first.next;
-			first.next = temp.next;
-			temp.next = res.next;
-			res.next = temp;
+			temp = first.next;// 1、temp保存当前第一个的下一个节点,temp=2，删除2 之前先保存2，避免断链
+			first.next = temp.next;// 2、将当前节点指向当前节点的下一个节点，类似删除2，first.next=3 ,新链表：[-1,1,3,4,5]
+			temp.next = res.next;// 将temp变为第一个节点，并将头节点指向temp,类似头插法，temp.next=1，temp：[1,3,4,5]
+			res.next = temp;// res->temp，temp:[2],2变为除(-1)外的头结点
 		}
 
 		return res.next;

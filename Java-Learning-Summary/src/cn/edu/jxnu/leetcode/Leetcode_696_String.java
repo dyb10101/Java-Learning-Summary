5ee4e409022659e5a92174d82630d9a06d304f13
jxnu-liguobin <568845948@qq.com>
2018-07-10 23:25:44 +0800

    添加696的另一个方法。。平PS：实习太忙

--- Java-Learning-Summary/src/cn/edu/jxnu/leetcode/Leetcode_696_String.java
+++ Java-Learning-Summary/src/cn/edu/jxnu/leetcode/Leetcode_696_String.java
@@ -1,5 +1,8 @@
 package cn.edu.jxnu.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  * 统计二进制字符串中连续 1 和连续 0 数量相同的子字符串个数
@@ -16,6 +19,17 @@ package cn.edu.jxnu.leetcode;
  */
 public class Leetcode_696_String {
 
+	public static void main(String[] args) {
+		String s1 = "000011111111000000011111111000";
+		int ret1 = 0;
+		int ret2 = 0;
+		ret1 = Leetcode_696_String.countBinarySubstrings(s1);
+		ret2 = Leetcode_696_String.countBinarySubstrings2(s1);
+		System.out.println(ret1);
+		System.out.println(ret2);
+
+	}
+
 	/**
 	 * 逻辑：每当我们在‘0’字符或‘1’字符之间切换时，我们都知道这是需要更新总数的时候。
 	 * 如果我们有如下顺序.。000011111111000(4个零，9个1，3个零)总数是4+3。
@@ -26,7 +40,7 @@ public class Leetcode_696_String {
 	 * @return
 	 *
 	 */
-	public int countBinarySubstrings(String s) {
+	public static int countBinarySubstrings(String s) {
 		int preLen = 0, curLen = 1, count = 0;
 		for (int i = 1; i < s.length(); i++) {
 			if (s.charAt(i) == s.charAt(i - 1)) {
@@ -41,6 +55,41 @@ public class Leetcode_696_String {
 			}
 		}
 		return count;
+	}
+
+	/**
+	 * 分析 可以发现如果是0011的话会包含2个，即0011 01 ；如果是000111的话包含3个，即000111 0011 01 ；01的话只有一个。
+	 * 也就是说有多少个重复的01即有多少个情况， 拿实例1来说 00110011 ： 可以找出三组 0011 1100 00011，每组都有2个，也就是6个。
+	 * 所以我们可以把字符串相邻的0,1个数统计出来 ，实例一可统计为： 2 2 2 ；相邻取比较小的那个值相加 为 2+2+2 = 6 再来一个例子
+	 * 000110000111 ： 统计每块儿的个数： 3 2 4 3 ；相邻较小数字相加：2+2+3 = 7
+	 * https://blog.csdn.net/qq_38595487/article/details/80280567
+	 * 
+	 * @param s
+	 * @return
+	 *
+	 */
+	public static int countBinarySubstrings2(String s) {
+
+		int result = 0;
+		List<Integer> list = new ArrayList<>();
+		int index = 0;
+		char temp = s.charAt(0);
+		list.add(1);
+		for (int i = 1; i < s.length(); i++) {
+			if (s.charAt(i) == temp) {
+				list.set(index, list.get(index) + 1);
+			} else {
+				list.add(1);
+				index++;
+				temp = s.charAt(i);
+			}
+		}
+
+		for (int i = 0; i < list.size() - 1; i++) {
+			result += Math.min(list.get(i), list.get(i + 1));
+		}
+
+		return result;
 	}
 
 }

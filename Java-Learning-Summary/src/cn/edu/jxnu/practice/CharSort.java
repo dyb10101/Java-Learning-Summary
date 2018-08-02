059ff991acd11cb47133eb003e7bb3ebfd8d7b5a
jxnu-liguobin <568845948@qq.com>
2018-08-03 00:00:43 +0800

    scala  二进制个位不同个数

--- Java-Learning-Summary/src/cn/edu/jxnu/practice/CharSort.java
+++ Java-Learning-Summary/src/cn/edu/jxnu/practice/CharSort.java
@@ -3,12 +3,20 @@ package cn.edu.jxnu.practice;
 import java.util.Scanner;
 
 /**
+ * 1、字符排序 【题目描述】输入一串字符，包含数字[0-9]和小写字母[a-z]，要求按数字从小到大、字母从 a 到 z 排序， 并且所有数字排在字母后面
+ * 
  * @author 梦境迷离.
  * @time 2018年8月2日
  * @version v1.0
  */
 public class CharSort {
 
+	/**
+	 * 变相的冒泡排序算法，判断条件追加字母与数字的区分
+	 * 
+	 * @param args
+	 *
+	 */
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String arg = scanner.nextLine();
